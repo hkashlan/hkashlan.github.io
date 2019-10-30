6b83e12a83a633649dc805b89b5d8c864c5625c1
hkashlan <hkashlan@gmail.com>
2019-10-30 19:18:56 +0100

    Add files via upload

---
+++ precache-manifest.0fa2f35907e80ab187c98f6057f62b1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a847ce033ff631c62902ac1de7055143",
+    "url": "/index.html"
+  },
+  {
+    "revision": "931224b3814985234771",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "88839dce3cd74454fa51",
+    "url": "/static/js/2.07a5e5cb.chunk.js"
+  },
+  {
+    "revision": "931224b3814985234771",
+    "url": "/static/js/main.e7e4965c.chunk.js"
+  },
+  {
+    "revision": "59895a03064c3b51980c",
+    "url": "/static/js/runtime-main.497bbed0.js"
+  }
+]);
