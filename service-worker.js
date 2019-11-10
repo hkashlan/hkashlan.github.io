05f72b4d6ff270fd0892f4036be2cf387c8d32f8
hkashlan <hkashlan@gmail.com>
2019-11-10 16:49:23 +0100

    add voice

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f41e8fa016acffb645d981dd4ca43cb1.js"
+  "/precache-manifest.9cbf05014290a5991f5d75ac8d2dc169.js"
 );
 
 self.addEventListener('message', (event) => {
