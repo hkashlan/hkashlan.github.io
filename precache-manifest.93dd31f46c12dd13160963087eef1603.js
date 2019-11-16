cf5fcad432a702f626bbd6054560944f46098f8f
hkashlan <hkashlan@gmail.com>
2019-11-16 13:55:34 +0100

    fix bug

---
+++ precache-manifest.93dd31f46c12dd13160963087eef1603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fa26a8f8f263b0415d58e823791399",
+    "url": "/index.html"
+  },
+  {
+    "revision": "572e7c19710874bbdaec",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c43c0b6b0c397f73e919",
+    "url": "/static/js/2.7c63b1a5.chunk.js"
+  },
+  {
+    "revision": "572e7c19710874bbdaec",
+    "url": "/static/js/main.9a2c8a32.chunk.js"
+  },
+  {
+    "revision": "59895a03064c3b51980c",
+    "url": "/static/js/runtime-main.497bbed0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba0d8c7abf5b2ba0322ffcfdf6160683.js"
+  "/precache-manifest.93dd31f46c12dd13160963087eef1603.js"
 );
 
 self.addEventListener('message', (event) => {
